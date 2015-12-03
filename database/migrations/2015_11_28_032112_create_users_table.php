9410a3e92a1380a606aa8a4b80222b8f8a37b1e1
Mary Gale <mary.jabagat@corp.mobile.co>
2015-12-03 12:13:39 +0800

    add other files

---
+++ database/migrations/2015_11_28_032112_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('username', 25);
+            $table->string('email', '250');
+            $table->tinyInteger('blocked', 4);
+            $table->tinyInteger('isSuperAdmin', 4);
+            $table->dateTime('registered_date');
+            $table->dateTime('last_visited');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    // Application Scripts
+    mix.scripts([
+        '../../../resources/scripts/app.js'
+    ], 'public/js/app.js');
+    mix.sass('app.scss');
+});
