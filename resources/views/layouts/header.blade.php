c508b112846785d6278ea64ed65b7d2c1aa87f1b
Mary Gale <mary.jabagat@corp.mobile.co>
2015-12-04 10:34:05 +0800

    layouts complete

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,49 @@
-<html>
+<!DOCTYPE html>
+<html lang="en-us">
 <head>
-    <title>State.org - @yield('title')</title>
+    <meta charset="utf-8">
 
-    <link href="{!! asset('css/bootstrap.min.css') !!}" media="screen,projection" rel="stylesheet" type="text/css" />
-    <link href="{!! asset('css/font-awesome.min.css') !!}" media="screen,projection" rel="stylesheet" type="text/css" />
-    <link href="{!! asset('css/smartadmin-production.css') !!}" media="screen,projection" rel="stylesheet" type="text/css" />
+    <title> SmartAdmin </title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Use the correct meta names below for your web application
+         Ref: http://davidbcalhoun.com/2010/viewport-metatag
+
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">-->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Basic Styles -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/font-awesome.min.css">
+
+    <!-- SmartAdmin Styles : Please note (smartadmin-production.css) was created using LESS variables -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/smartadmin-production.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/smartadmin-skins.css">
+
+    <!-- SmartAdmin RTL Support is under construction
+    <link rel="stylesheet" type="text/css" media="screen" href="css/smartadmin-rtl.css"> -->
+
+    <!-- We recommend you use "your_style.css" to override SmartAdmin
+         specific styles this will also ensure you retrain your customization with each SmartAdmin update.
+    <link rel="stylesheet" type="text/css" media="screen" href="css/your_style.css"> -->
+
+    <!-- Demo purpose only: goes with demo.js, you can delete this css when designing your own WebApp -->
+    <link rel="stylesheet" type="text/css" media="screen" href="css/demo.css">
+
+    <!-- FAVICONS -->
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon/favicon.ico" type="image/x-icon">
+
+    <!-- GOOGLE FONT -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+
 </head>
 <body class="">
+<!-- possible classes: minified, fixed-ribbon, fixed-header, fixed-width-->
+
 <!-- HEADER -->
 <header id="header">
     <div id="logo-group">
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,4 @@
+<!-- Left panel : Navigation area -->
 <aside id="left-panel">
 
     <!-- User info -->
@@ -5,9 +6,9 @@
 				<span> <!-- User image size is adjusted inside CSS, it should stay as it -->
 
 					<a href="javascript:void(0);" id="show-shortcut">
-                        <img src="img/avatars/sunny.png" alt="me" class="online" />
+                        <img src="img/avatars/1.png" alt="me" class="online" />
 						<span>
-							john.doe
+							mary.gale
 						</span>
                         <i class="fa fa-angle-down"></i>
                     </a>
@@ -15,53 +16,38 @@
 				</span>
     </div>
     <!-- end user info -->
-
-    <!-- NAVIGATION : This navigation is also responsive
-
-    To make this navigation dynamic please make sure to link the node
-    (the reference to the nav > ul) after page load. Or the navigation
-    will not initialize.
-    -->
     <nav>
-        <!-- NOTE: Notice the gaps after each icon usage <i></i>..
-        Please note that these links work a bit different than
-        traditional hre="" links. See documentation for details.
-        -->
-
         <ul>
             <li>
-                <a href="index.html" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>
+                <a href="/" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Dashboard</span></a>
             </li>
             <li>
-                <a href="create_user" title="Add new user"><i class="fa fa-lg fa-fw fa-pencil-square-o"></i> Create User</a>
-            </li>
-            {{--<li>
-                <a href="inbox.html"><i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Inbox</span><span class="badge pull-right inbox-badge">14</span></a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-lg fa-fw fa-bar-chart-o"></i> <span class="menu-item-parent">Graphs</span></a>
+                <a href="#"><i class="fa fa-lg fa-fw fa-user"></i> <span class="menu-item-parent">Users</span></a>
                 <ul>
                     <li>
-                        <a href="flot.html">Flot Chart</a>
+                        <a href="user/allusers">All Users</a>
                     </li>
                     <li>
-                        <a href="morris.html">Morris Charts</a>
+                        <a href="users/newusers">New Users</a>
                     </li>
                     <li>
-                        <a href="inline-charts.html">Inline Charts</a>
+                        <a href="users/profile">Your Profile</a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="#"><i class="fa fa-lg fa-fw fa-table"></i> <span class="menu-item-parent">Tables</span></a>
+                <a href="#"><i class="fa fa-lg fa-fw fa-file"></i> <span class="menu-item-parent">Pages</span></a>
                 <ul>
                     <li>
-                        <a href="table.html">Normal Tables</a>
+                        <a href="forum.html">All Pages</a>
                     </li>
                     <li>
-                        <a href="datatables.html">Data Tables</a>
+                        <a href="profile.html">Add New</a>
                     </li>
                 </ul>
+            </li>
+            <li>
+                <a href="inbox"><i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Inbox</span><span class="badge pull-right inbox-badge">14</span></a>
             </li>
             <li>
                 <a href="#"><i class="fa fa-lg fa-fw fa-pencil-square-o"></i> <span class="menu-item-parent">Forms</span></a>
@@ -93,95 +79,6 @@
                 </ul>
             </li>
             <li>
-                <a href="#"><i class="fa fa-lg fa-fw fa-desktop"></i> <span class="menu-item-parent">UI Elements</span></a>
-                <ul>
-                    <li>
-                        <a href="general-elements.html">General Elements</a>
-                    </li>
-                    <li>
-                        <a href="buttons.html">Buttons</a>
-                    </li>
-                    <li>
-                        <a href="#">Icons</a>
-                        <ul>
-                            <li>
-                                <a href="fa.html"><i class="fa fa-plane"></i> Font Awesome</a>
-                            </li>
-                            <li>
-                                <a href="glyph.html"><i class="glyphicon glyphicon-plane"></i> Glyph Icons </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="grid.html">Grid</a>
-                    </li>
-                    <li>
-                        <a href="treeview.html">Tree View</a>
-                    </li>
-                    <li>
-                        <a href="nestable-list.html">Nestable Lists</a>
-                    </li>
-                    <li>
-                        <a href="jqui.html">JQuery UI</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-lg fa-fw fa-folder-open"></i> <span class="menu-item-parent">6 Level Navigation</span></a>
-                <ul>
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-folder-open"></i> 2nd Level</a>
-                        <ul>
-                            <li>
-                                <a href="#"><i class="fa fa-fw fa-folder-open"></i> 3ed Level </a>
-                                <ul>
-                                    <li>
-                                        <a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-fw fa-folder-open"></i> 4th Level</a>
-                                        <ul>
-                                            <li>
-                                                <a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
-                                            </li>
-                                            <li>
-                                                <a href="#"><i class="fa fa-fw fa-folder-open"></i> 5th Level</a>
-                                                <ul>
-                                                    <li>
-                                                        <a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-folder-open"></i> Folder</a>
-
-                        <ul>
-                            <li>
-                                <a href="#"><i class="fa fa-fw fa-folder-open"></i> 3ed Level </a>
-                                <ul>
-                                    <li>
-                                        <a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-fw fa-file-text"></i> File</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                    </li>
-                </ul>
-            </li>
-            <li>
                 <a href="calendar.html"><i class="fa fa-lg fa-fw fa-calendar"><em>3</em></i> <span class="menu-item-parent">Calendar</span></a>
             </li>
             <li>
@@ -189,50 +86,6 @@
             </li>
             <li>
                 <a href="gallery.html"><i class="fa fa-lg fa-fw fa-picture-o"></i> <span class="menu-item-parent">Gallery</span></a>
-            </li>
-            <li>
-                <a href="gmap-xml.html"><i class="fa fa-lg fa-fw fa-map-marker"></i> <span class="menu-item-parent">Google Map Skins</span><span class="badge bg-color-greenLight pull-right inbox-badge">9</span></a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-lg fa-fw fa-windows"></i> <span class="menu-item-parent">Miscellaneous</span></a>
-                <ul>
-                    <li>
-                        <a href="typography.html">Typography</a>
-                    </li>
-                    <li>
-                        <a href="pricing-table.html">Pricing Tables</a>
-                    </li>
-                    <li>
-                        <a href="invoice.html">Invoice</a>
-                    </li>
-                    <li>
-                        <a href="login.html" target="_top">Login</a>
-                    </li>
-                    <li>
-                        <a href="register.html" target="_top">Register</a>
-                    </li>
-                    <li>
-                        <a href="lock.html" target="_top">Locked Screen</a>
-                    </li>
-                    <li>
-                        <a href="error404.html">Error 404</a>
-                    </li>
-                    <li>
-                        <a href="error500.html">Error 500</a>
-                    </li>
-                    <li class="active">
-                        <a href="blank_.html">Blank Page</a>
-                    </li>
-                    <li>
-                        <a href="email-template.html">Email Template</a>
-                    </li>
-                    <li>
-                        <a href="search.html">Search Page</a>
-                    </li>
-                    <li>
-                        <a href="ckeditor.html">CK Editor</a>
-                    </li>
-                </ul>
             </li>
             <li>
                 <a href="#"><i class="fa fa-lg fa-fw fa-file"></i> <span class="menu-item-parent">Other Pages</span></a>
@@ -247,7 +100,7 @@
                         <a href="timeline.html">Timeline</a>
                     </li>
                 </ul>
-            </li>--}}
+            </li>
         </ul>
     </nav>
     <span class="minifyme"> <i class="fa fa-arrow-circle-left hit"></i> </span>
