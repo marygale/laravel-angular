42fb40b66dcca15758a3fe2d2a0fc124cc9018a7
marygale <marygale043@gmail.com>
2016-07-14 15:33:08 +0800

    fix login and logout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -92,7 +92,7 @@
 
         <!-- logout button -->
         <div id="logout" class="btn-header transparent pull-right">
-            <span> <a href="{{ action('Auth\AuthController@logout') }}}" title="Sign Out"><i class="fa fa-sign-out"></i></a> </span>
+            <span> <a href="{{ action('Auth\AuthController@logout') }}" title="Sign Out"><i class="fa fa-sign-out"></i></a> </span>
         </div>
         <!-- end logout button -->
 
